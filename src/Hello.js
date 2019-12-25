1728f4aca6def145b081a88edcb788d5e4016f3b
mihir-codes <mihiesinh.parmar.it@gmail.com>
2019-12-25 12:58:41 +0530

    First Commit

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import './Hello.css';
+
+class Hello extends Component{
+    render(){
+        return <h1 className='f1 tc'>Hello World</h1>
+    }
+}
+
+export default Hello;
