bb735bf197cad6667e20603bc9e134b0c13dbed8
mihir-codes <mihiesinh.parmar.it@gmail.com>
2019-12-29 11:38:20 +0530

    Using middleware for async calls

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,38 +1,48 @@
 import CardList from "../Components/CardList";
 import React,{Component} from 'react';
+import {connect} from 'react-redux';
 import Title from '../Components/Title';
 import SearchBar from '../Components/SearchBar';
 import Scroll from '../Components/Scroll';
+import {setSearchField, fetchingRobots} from "../actions";
+
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        isPending: state.fetchRobots.isPending,
+        robots: state.fetchRobots.robots,
+        error: state.fetchRobots.error,
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onFetchRobots: () => dispatch(fetchingRobots())
+    }
+};
 
 class App extends Component{
-    constructor() {
-        super();
-        this.state={
-            robots: [],
-            searchField: ''
-        }
-    }
+
     componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-             .then(response=>response.json())
-             .then(users=>{this.setState({robots: users})});
+        this.props.onFetchRobots();
     }
 
-    onSearchChange=(event)=>{
-        this.setState({searchField: event.target.value});
-        };
     render (){
-        const {robots,searchField} = this.state;
+        const {searchField, onSearchChange, robots, isPending} = this.props;
         const filteredRobots = robots.filter((robot) => (robot.name.toLowerCase().includes(searchField.toLowerCase())));
         return (
-        <div className={"tc"}>
-            <Title/>
-            <SearchBar searchChange={this.onSearchChange}/>
-            <Scroll>
-                <CardList robots={filteredRobots}/>
-            </Scroll>
-        </div>
+            <div className={"tc"}>
+                <Title/>
+                <SearchBar searchChange={onSearchChange}/>
+                {isPending?
+                    <h1>Loading</h1>
+                    :<Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>}
+            </div>
     )}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Container/App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from "redux";
+import {logger} from "redux-logger";
+import thunkMiddleware from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './Container/App';
+import {searchRobots, fetchRobots} from "./reducer";
 
-ReactDOM.render(<App/>
+const rootReducer = combineReducers({searchRobots, fetchRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+ReactDOM.render(<Provider store={store}><App/></Provider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
