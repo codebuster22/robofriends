fc60dac5b55395d89c4132a34e810bdef9755c74
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-04-13 14:47:03 +0530

    Updates

---
+++ precache-manifest.da9b5de12293bf02ceecd598455adecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f72a6b22aea68520d04adc29deb7d77",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7754a50055fc8816bc2b",
+    "url": "/robofriends/static/css/2.d5377655.chunk.css"
+  },
+  {
+    "revision": "a0ef386a504021f631db",
+    "url": "/robofriends/static/css/main.db3cc107.chunk.css"
+  },
+  {
+    "revision": "7754a50055fc8816bc2b",
+    "url": "/robofriends/static/js/2.79950c9c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/robofriends/static/js/2.79950c9c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0ef386a504021f631db",
+    "url": "/robofriends/static/js/main.5a9bd28f.chunk.js"
+  },
+  {
+    "revision": "04454491b22da9ad3e7d",
+    "url": "/robofriends/static/js/runtime-main.98228a54.js"
+  }
+]);
